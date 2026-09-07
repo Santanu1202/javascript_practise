5b9c9d336933b8a8b742e83b95c23d8df60d3ff3
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-07 18:51:26 +0000

    varriable learn

---
+++ basic/varriable.js
@@ -0,0 +1,15 @@
+const accountId = 253666
+let accountEmail = "santani@gmail.com"
+var accountPassword = "2589"
+accountcity = "bankura"
+let accountState;
+
+/*not used var */
+//accountId = 5    //not allowed 
+
+accountEmail = "sxfds@gmail.com"
+accountPassword = "5555"
+accountCity = "bangalore"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
