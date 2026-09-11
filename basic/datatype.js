d695b0b468cbeec74b13d64512030a3be91717ad
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-11 18:26:30 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,11 @@
+"user strict"; // newer version of js 
+
+// primitive datatype 
+//number = 2 to power53 
+//bigint 
+//string =" "
+//null  standlone value  empty 
+// undefined  when  we declare varriable but not value 
+// symbol finding uniqueness 
+
+//object 
