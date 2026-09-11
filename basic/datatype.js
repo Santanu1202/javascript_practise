3a20c65f86ba15bb0c6f1a6a4e55c95e40de9cb2
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-11 19:01:49 +0000

    Conversion

--- basic/datatype.js
+++ basic/datatype.js
@@ -9,3 +9,30 @@
 // symbol finding uniqueness 
 
 //object 
+
+
+//string to number 
+let score = "3f3"
+console.log(typeof score);
+
+let valueInNumber = Number(score) // change a string into number 
+console.log(typeof valueInNumber);
+console.log(valueInNumber) 
+
+// "33"-> 33 convert easily 
+//"33abc"-> NaN  //true -> 1; false-> 0
+
+//boolean conversion 
+//let isLoggedIn = "santanu"   returrn true 
+let isLoggedIn = ""    //false return 
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+
+//number to string conversion 
+let someNumber = 78
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
