e7d16cdbdc295790335b0421aef0fbdce708807d
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-15 06:36:44 +0000

    postfix & prefix

---
+++ basic/opera.js
@@ -0,0 +1,8 @@
+let x = 2;
+let y = x++;
+console.log(`x:${x},y:${y}`);
+///postfix increment at first increment then print   old value first increment laterr 
+let a = 5;
+let b = ++a;
+console.log(`a:${a},b:${b}`);
+///prefix increment  first increase then use
