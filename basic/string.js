06e2f3a74df8f9862d2a98c3c0ebd3f10a95a588
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-16 16:55:50 +0000

    strings

---
+++ basic/string.js
@@ -0,0 +1,7 @@
+const myName = "Santanu"
+const age = "26"
+ console.log(`My name is ${myName} and my age is ${age}`);     
+ // here ${} use for varriable and modern days use for the backtricks
+ console.log(`My name is ${myName.toUpperCase()} and my age is ${age}`);      
+// using this ${} we use any function for operation 
+
