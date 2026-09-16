caca9b43b6703e26425855727b702c36454b9adc
Santanu Rana <132618738+Santanu1202@users.noreply.github.com>
2026-09-16 17:30:38 +0000

    function

--- basic/string.js
+++ basic/string.js
@@ -3,5 +3,96 @@ const age = "26"
  console.log(`My name is ${myName} and my age is ${age}`);     
  // here ${} use for varriable and modern days use for the backtricks
  console.log(`My name is ${myName.toUpperCase()} and my age is ${age}`);      
-// using this ${} we use any function for operation 
+// using this ${} we use any function for operation like (touppercase),(proto),
 
+let str = "Hello JavaScript";
+
+// 1. length
+// Task: Find the total number of characters in the string.
+
+
+// 2. toUpperCase()
+// Task: Convert the complete string into uppercase letters.
+
+
+// 3. toLowerCase()
+// Task: Convert the complete string into lowercase letters.
+
+
+// 4. charAt()
+// Task: Get the character at index number 4.
+
+
+// 5. at()
+// Task: Get the last character of the string using at().
+
+
+// 6. includes()
+// Task: Check whether the string contains the word "JavaScript".
+// Expected result: true
+
+
+// 7. startsWith()
+// Task: Check whether the string starts with "Hello".
+// Expected result: true
+
+
+// 8. endsWith()
+// Task: Check whether the string ends with "Script".
+// Expected result: true
+
+
+// 9. indexOf()
+// Task: Find the index/position where "JavaScript" starts.
+
+
+// 10. slice()
+// Task: Extract "Hello" from the string using slice().
+
+
+// 11. replace()
+// Task: Replace "JavaScript" with "World".
+
+
+// 12. trim()
+// Task: Remove the extra spaces from the beginning and end
+// of this string: "   Hello JavaScript   "
+
+
+// 13. split()
+// Task: Split the string "HTML,CSS,JavaScript" into an array
+// using comma (,) as the separator.
+
+
+
+// 14. replaceAll()
+// Task: Replace every occurrence of "a" with "x"
+// in the string "Java and JavaScript".
+
+
+// 15. substring()
+// Task: Extract "Java" from "Hello JavaScript"
+// using substring().
+
+
+// 16. concat()
+// Task: Join "Hello" and " JavaScript" using concat().
+
+
+// 17. repeat()
+// Task: Repeat the string "Hello " 3 times.
+
+
+// 18. lastIndexOf()
+// Task: Find the position of the LAST occurrence of
+// the letter "a" in "JavaScript".
+
+
+// 19. trimStart()
+// Task: Remove only the spaces from the START of:
+// "   Hello JavaScript   "
+
+
+// 20. trimEnd()
+// Task: Remove only the spaces from the END of:
+// "   Hello JavaScript   "
